677941d1fc033e868dd1c5d2ccad52ed6f6d8a96
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-22 20:53:22 +0530

    fix: add message

--- binaries/debugon.sh
+++ binaries/debugon.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if "$DEBUG"; then
-	echo "DEBUG mode on."
-	set -xve
-	PS4='+ ${BASH_SOURCE}:${LINENO}:${FUNCNAME[0]}() '
+  printf "DEBUG mode on.\n"
+  set -xve
+  PS4='+ ${BASH_SOURCE}:${LINENO}:${FUNCNAME[0]}() '
 fi
