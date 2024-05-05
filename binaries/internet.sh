bf04b7bf6c9ee65f6df898a4e476412f0e45fe71
Linus Fernandes <linus.fernandes@gmail.com>
2024-05-05 21:11:43 +0530

    fix: update git scripts for check internet connection

--- binaries/internet.sh
+++ binaries/internet.sh
@@ -2,7 +2,7 @@
 
 ######################################################################
 # @author      : Linus Fernandes (linusfernandes at gmail dot com)
-# @file        : internet
+# @file        : internet.sh
 # @created     : Tuesday Mar 26, 2024 11:57:10 IST
 #
 # @description :
