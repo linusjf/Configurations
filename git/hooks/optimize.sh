193530be5e61151688bb33f948e9bfa12f8a19b0
Linus Fernandes <linus.fernandes@gmail.com>
2025-03-05 16:41:44 +0530

    refactor: refactor pre-commit hook. Refactor optimize methods.

--- git/hooks/optimize.sh
+++ git/hooks/optimize.sh
@@ -5,21 +5,24 @@ optimize() {
   mode=$(stat -c "%a" "$file")
   jpegoptim -f -s "$file" \
     && hash=$(git hash-object -w "${file}") \
-    &&
-    # Add it back to index \
-    git update-index --add --cacheinfo "100${mode}" "$hash" "$file" || exit 1
+    && git update-index --add --cacheinfo "100${mode}" "$hash" "$file" || exit 1
   return $?
 }
 
 optimizejpgs() {
   local ret=0
-  exts="'.*(\.jpg$|\.jpeg$)'"
-  cmd="git diff --cached --name-only --diff-filter=ACMR | grep -o -E $exts"
+  local exts=".*\.(jpg|jpeg)$"
+  local cmd="git diff --cached --name-only --diff-filter=ACMR | grep -E \"$exts\""
+
   readarray -t FILES < <(eval "$cmd")
-  ret=$((ret + $?))
+  ret=$?
+
+  if [[ ${#FILES[@]} -eq 0 ]]; then return 0; fi
+
   for file in "${FILES[@]}"; do
     optimize "$file"
     ret=$((ret + $?))
   done
+
   return "$ret"
 }
