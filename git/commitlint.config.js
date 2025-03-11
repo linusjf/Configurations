bdd41c12beaf9a90bfc75d8d5c115c533a7efa2b
Linus Fernandes <linus.fernandes@gmail.com>
2025-03-11 11:07:28 +0530

    fix: update SpaceVim config and disable javascript layer since it causes problems with tern

--- git/commitlint.config.js
+++ git/commitlint.config.js
@@ -3,12 +3,27 @@
  * Copyright (C) 2021  <@localhost>
  *
  * Distributed under terms of the MIT license.
- */
+
+*/
 module.exports = {
-    extends: [
-        "@commitlint/config-conventional"
-    ],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-        "type-enum": [2, "always", ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test"]],
-    }
-}
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+  },
+};
