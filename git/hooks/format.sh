1e94f672cd6f0f7ec51ab4f1acf1be959befc047
Linus Fernandes <linus.fernandes@gmail.com>
2025-03-23 08:44:07 +0530

    fix: add return code capture

--- git/hooks/format.sh
+++ git/hooks/format.sh
@@ -32,26 +32,38 @@ format_file() {
 }
 
 format_go() {
+  local ret=0
   local -a files=()
   readarray -t files < <(git diff --cached --name-only --diff-filter=ACMR | grep '\.go$')
   for file in "${files[@]}"; do
-    format_file "$file" gofmt
+    format_file "$file" gofmt || ((ret = 1))
   done
+  exit "$ret"
 }
 
 format_json() {
   local ret=0
   local -a files=()
-  readarray -t files < <(git diff --cached --name-only --diff-filter=ACMR | grep '(\.json)|(^\.*rc)$')
+  readarray -t files < <(git diff --cached --name-only --diff-filter=ACMR | grep '\.json$')
   for file in "${files[@]}"; do
     if isjsonfile "$file"; then
-      format_file "$file" json --quiet --output jsony -I -f
+      format_file "$file" json --quiet --output jsony -I -f || ((ret = 1))
+    else
+      printf "Invalid json file: %s" "$file"
+      ((ret = 1))
+    fi
+  done
+  readarray -t files < <(git diff --cached --name-only --diff-filter=ACMR | grep '^\.*rc$')
+  for file in "${files[@]}"; do
+    if isjsonfile "$file"; then
+      format_file "$file" json --quiet --output jsony -I -f || ((ret = 1))
     fi
   done
   return "$ret"
 }
 
 format_xml() {
+  local ret=0
   local tidy_path
   tidy_path=$(command -v tidy)
   if [ -z "$tidy_path" ]; then
@@ -62,8 +74,9 @@ format_xml() {
   local -a files=()
   readarray -t files < <(git diff --cached --name-only --diff-filter=ACMR | grep '\.xml$')
   for file in "${files[@]}"; do
-    format_file "$file" "$tidy_path"
+    format_file "$file" "$tidy_path" || ((ret = 1))
   done
+  exit "$ret"
 }
 
 formatandcheck() {
